ee5e925da58a3bb7c51a34414677282e884b7731
LouiIII3 <119919129+LouiIII3@users.noreply.github.com>
2024-05-13 09:49:04 +0900

    Create CustomUserDetailsService.java

---
+++ service/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.example.springjwt.service;
+
+import com.example.springjwt.dto.CustomUserDetails;
+import com.example.springjwt.entitiy.UserEntity;
+import com.example.springjwt.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+
+            return new CustomUserDetails(userData);
+        }
+
+
+        return null;
+    }
+}
